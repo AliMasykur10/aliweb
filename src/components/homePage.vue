0576a313efeca95b95ba6cf5a5792987308cc265
AliMasykur10 <alimasykur2001@gmail.com>
2022-11-20 16:04:55 +0700

    hompage & navbar awal

---
+++ src/components/homePage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="homepage">
+    <h1>Welcome to My persnoal website</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "homePage",
+};
+</script>
+
+<style>
+.homepage {
+  width: 100vw;
+  height: 100vh;
+  background-color: aqua;
+}
+</style>
---
+++ src/components/navBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="navbar">
+    <ul>
+      <li><a href="">Home</a></li>
+      <li><a href="">About</a></li>
+      <li><a href="">Project</a></li>
+      <li><a href="">Contact</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "navBar",
+};
+</script>
+
+<style>
+li {
+  list-style: none;
+  margin-left: 1rem;
+}
+a {
+  text-decoration: none;
+}
+
+.navbar ul {
+  display: flex;
+  background-color: bisque;
+  padding: 0.5rem;
+}
+</style>
